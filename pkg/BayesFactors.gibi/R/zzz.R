114c44a065d4db9c022ba221f888f0517314f310
richarddmorey <richarddmorey@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-04 15:36:40 +0000

    added main plugin structure. Lacks any real code to do anything.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/BayesFactors.gibi/R/zzz.R
@@ -0,0 +1,14 @@
+.onLoad <- function(libname, pkgname)
+{
+	# define version numbers
+	allPackages = installed.packages()
+
+	.pluginVersion <<- allPackages[which(allPackages[,1]==pkgname),3]
+	.gibiVersion <<- allPackages[which(allPackages[,1]=="gibi"),3]
+	.pluginName <<- pkgname	 
+	
+	
+	.pluginEnv <<- new.env(parent=baseenv())
+	initialEnvironment(.pluginEnv)
+
+}
